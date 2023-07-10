a853ee4638449e0c0b9cc118957bc72abc2d68ea
Ochiengsteven <stevenochieng6305@gmail.com>
2023-07-10 21:36:39 +0300

    add html framework

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@
   \*************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `* {\r\n  margin: 0;\r\n  padding: 0;\r\n  box-sizing: border-box;\r\n}\r\n\r\nh3 {\r\n  font-size: 1.5rem;\r\n  color: red;\r\n}\r\n\r\nimg {\r\n  width: 60%;\r\n  height: 40vh;\r\n  object-fit: cover;\r\n}`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://to-do/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `* {\r\n  margin: 0;\r\n  padding: 0;\r\n  box-sizing: border-box;\r\n}\r\n\r\nmain {\r\n  display: flex;\r\n  flex-direction: column;\r\n  align-items: center;\r\n  justify-content: center;\r\n  height: 100vh;\r\n}\r\n\r\n.container {\r\n  display: flex;\r\n  flex-direction: column;\r\n  align-items: center;\r\n  width: 50%;\r\n  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);\r\n}\r\n\r\n.header {\r\n  width: 100%;\r\n  display: flex;\r\n  justify-content: space-between;\r\n  align-items: center;\r\n  padding: 1rem;\r\n  border-bottom: 1px solid #ccc;\r\n}\r\n\r\n.header h1 {\r\n  font-size: 1.2rem;\r\n  font-weight: 600;\r\n}\r\n\r\n.task {\r\n  width: 100%;\r\n  display: flex;\r\n  justify-content: flex-start;\r\n  align-items: center;\r\n  padding: 1rem;\r\n  border-bottom: 1px solid #ccc;\r\n}\r\n\r\n.task p {\r\n  font-size: 1rem;\r\n  font-weight: 500;\r\n  margin-left: 1rem;\r\n}\r\n\r\n.header img,\r\n.task img {\r\n  width: 20px;\r\n  height: 20px;\r\n}\r\n\r\nform {\r\n  width: 100%;\r\n}\r\n\r\ninput {\r\n  width: 100%;\r\n  padding: 1rem;\r\n  border: none;\r\n  outline: none;\r\n  font-size: 1rem;\r\n  border-bottom: 1px solid #ccc;\r\n}\r\n\r\n.clear-all {\r\n  width: 100%;\r\n  display: flex;\r\n  justify-content: center;\r\n  align-items: center;\r\n  padding: 0.8rem;\r\n  background: #fafafa;\r\n}\r\n\r\n.clear-all p:hover {\r\n  cursor: pointer;\r\n  text-decoration: underline;\r\n}`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://to-do/./src/style.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -116,7 +116,27 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nconst frame = document.querySelector('.container');\n\nconst tasks = [\n  {\n    id: 1,\n    title: 'Task 1',\n    description: 'Description 1',\n  },\n];\n\n\n//# sourceURL=webpack://to-do/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _assets_reload_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./assets/reload.png */ \"./src/assets/reload.png\");\n/* harmony import */ var _assets_unchecked_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./assets/unchecked.png */ \"./src/assets/unchecked.png\");\n\n\n\n\nconst reloadIcon = new Image();\nreloadIcon.src = _assets_reload_png__WEBPACK_IMPORTED_MODULE_1__;\n\nconst uncheckIcon = new Image();\nuncheckIcon.src = _assets_unchecked_png__WEBPACK_IMPORTED_MODULE_2__;\n\nconst frame = document.querySelector('.container');\nconst header = document.createElement('div');\nheader.classList.add('header');\nheader.innerHTML = '<h1>Todo List</h1>';\nheader.appendChild(reloadIcon);\nframe.appendChild(header);\nconst taskForm = document.createElement('form');\ntaskForm.classList.add('task-form');\ntaskForm.innerHTML = '<input type=\"text\" class=\"task-input\" placeholder=\"Add to your list...\">';\nframe.appendChild(taskForm);\n\nconst tasks = [\n  {\n    description: 'Finish homework',\n    completed: false,\n    index: 1,\n  },\n  {\n    description: 'Go grocery shopping',\n    completed: false,\n    index: 2,\n  },\n  {\n    description: 'Clean the house',\n    completed: false,\n    index: 3,\n  },\n];\n\ntasks.forEach((task) => {\n  const div = document.createElement('div');\n  div.classList.add('task');\n  div.innerHTML = `<img src=\"${uncheckIcon.src}\" alt=\"Uncheck Icon\"><p>${task.description}</p>`;\n  frame.appendChild(div);\n});\n\nconst clearAll = document.createElement('div');\nclearAll.classList.add('clear-all');\nclearAll.innerHTML = '<p>Clear All Completed</p>';\nframe.appendChild(clearAll);\n\n\n//# sourceURL=webpack://to-do/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/assets/reload.png":
+/*!*******************************!*\
+  !*** ./src/assets/reload.png ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"573f8bf282d92beace11.png\";\n\n//# sourceURL=webpack://to-do/./src/assets/reload.png?");
+
+/***/ }),
+
+/***/ "./src/assets/unchecked.png":
+/*!**********************************!*\
+  !*** ./src/assets/unchecked.png ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"dbcaee77233fbee6f529.png\";\n\n//# sourceURL=webpack://to-do/./src/assets/unchecked.png?");
 
 /***/ })
 
@@ -171,6 +191,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -185,6 +217,29 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 import './style.css';
+import reload from './assets/reload.png';
+import uncheck from './assets/unchecked.png';
+
+const reloadIcon = new Image();
+reloadIcon.src = reload;
+
+const uncheckIcon = new Image();
+uncheckIcon.src = uncheck;
 
 const frame = document.querySelector('.container');
+const header = document.createElement('div');
+header.classList.add('header');
+header.innerHTML = '<h1>Todo List</h1>';
+header.appendChild(reloadIcon);
+frame.appendChild(header);
+const taskForm = document.createElement('form');
+taskForm.classList.add('task-form');
+taskForm.innerHTML = '<input type="text" class="task-input" placeholder="Add to your list...">';
+frame.appendChild(taskForm);
 
 const tasks = [
   {
-    id: 1,
-    title: 'Task 1',
-    description: 'Description 1',
+    description: 'Finish homework',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go grocery shopping',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Clean the house',
+    completed: false,
+    index: 3,
   },
 ];
+
+tasks.forEach((task) => {
+  const div = document.createElement('div');
+  div.classList.add('task');
+  div.innerHTML = `<img src="${uncheckIcon.src}" alt="Uncheck Icon"><p>${task.description}</p>`;
+  frame.appendChild(div);
+});
+
+const clearAll = document.createElement('div');
+clearAll.classList.add('clear-all');
+clearAll.innerHTML = '<p>Clear All Completed</p>';
+frame.appendChild(clearAll);
