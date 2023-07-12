c6ac3da071723bb868f0d996c7c55be74f1d56fe
Ochiengsteven <stevenochieng6305@gmail.com>
2023-07-12 14:02:03 +0300

    add the remove functions

--- src/index.js
+++ src/index.js
@@ -31,23 +31,23 @@ let tasks = [];
 
 const form = document.querySelector('.task-form');
 
+const addedTasks = document.createElement('div');
+addedTasks.classList.add('added-tasks');
+
+frame.appendChild(addedTasks);
 const displayTasks = () => {
-  const addedTasks = document.createElement('div');
-  addedTasks.classList.add('added-tasks');
   addedTasks.innerHTML = '';
-  frame.appendChild(addedTasks);
+
   tasks.forEach((task) => {
     const div = document.createElement('div');
     div.classList.add('task');
-    div.setAttribute('id', task.index);
-    div.innerHTML = `<img src="${uncheckIcon.src}" alt="Uncheck Icon"><p>${task.description}</p><img src="${deleteIcon.src}" alt="deleteico" class="deleteico"><img src="${dotsIcon.src}" alt="move" class="move">`;
+    div.setAttribute('id', `task-${task.index}`);
+    div.innerHTML = `<img src="${uncheckIcon.src}" alt="Uncheck Icon">
+                     <p>${task.description}</p>
+                     <img src="${deleteIcon.src}" alt="deleteico" class="deleteico">
+                     <img src="${dotsIcon.src}" alt="move" class="move">`;
     addedTasks.appendChild(div);
   });
-
-  const clearAll = document.createElement('div');
-  clearAll.classList.add('clear-all');
-  clearAll.innerHTML = '<p>Clear All Completed</p>';
-  frame.appendChild(clearAll);
 
   localStorage.setItem('tasks', JSON.stringify(tasks));
 };
@@ -58,32 +58,32 @@ form.addEventListener('submit', (e) => {
   const myTask = {
     description: inputTask,
     completed: false,
-    index: tasks.length + 1,
+    index: Math.floor(Math.random() * 1000), // Generate a random number as the index
   };
   tasks.push(myTask);
   document.querySelector('.task-input').value = '';
   displayTasks();
 });
 
-const deleteTask = (e) => {
-  const index = e.target.parentNode.id;
-  tasks.splice(index - 1, 1);
-  frame.innerHTML = '';
-  displayTasks();
-};
+const clearAll = document.createElement('div');
+clearAll.classList.add('clear-all');
+clearAll.innerHTML = '<p>Clear All Completed</p>';
+frame.appendChild(clearAll);
 
-document.addEventListener('click', (e) => {
-  if (e.target.className === 'deleteico') {
-    deleteTask(e);
+addedTasks.addEventListener('click', (event) => {
+  if (event.target.classList.contains('deleteico')) {
+    const taskId = Number(event.target.parentNode.id.split('-')[1]);
+    tasks = tasks.filter((task) => task.index !== taskId);
+    displayTasks();
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   const storedTasks = localStorage.getItem('tasks');
 
-  // If there's any data, parse it and assign it to bookData
   if (storedTasks) {
     tasks = JSON.parse(storedTasks);
-    displayTasks(); // Display the books from the local storage data
+    displayTasks();
   }
 });
